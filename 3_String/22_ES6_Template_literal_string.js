d58102f72ef7cbdf43927eb4410de2fd82f0363a
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-07 17:03:30 +0500

    ES6 Template Literal String

---
+++ 3_String/22_ES6_Template_literal_string.js
@@ -0,0 +1,12 @@
+// Template Literal gives a way to display the expression inside the backtick (`) itself
+
+let a = 90;
+let str = `Value of a is ${a}`;
+console.log(str); // Value of a is 90
+
+//  Template String helps us to store the format as it is then escape sequence is not required
+// for special characters/spaces/new lines
+let str2 = `This is
+     second line and
+	             this is the third one`;
+console.log(str2);
