f1d04d2bccccd49d417706eb1fe4b3e94860c4cf
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-10 09:41:14 +0500

    Difference between Document Object Window Object

---
+++ 7_DOM_Web/40_Window_Document_Object.js
@@ -0,0 +1,22 @@
+// Window - Main Container/Parent/Global Object
+
+// Document Object is child of Window Object
+
+// All the members like object, methods or properties if they are
+// part of window object then we do not refer the window object
+// i,e, window.document
+// setInterval() or setTimeout() etc., are methods of "window"
+// Document is an object of "window"
+
+// Document object does have various object methods or properties like,
+// document.getElementById()
+
+// Window object has properties like: innerHeight/innerWidth
+
+console.log(window.innerHeight);
+console.log(innerWidth);
+console.log(window.location);
+console.log(location.href);
+
+// DOM - Document Object Model
+// BOM - Browser Object Model
