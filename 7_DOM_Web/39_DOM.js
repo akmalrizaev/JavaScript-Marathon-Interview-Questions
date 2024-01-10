11ae974f247b815bc02862a9d13d94f242755a89
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-10 09:12:05 +0500

    Underatanding DOM

---
+++ 7_DOM_Web/39_DOM.js
@@ -0,0 +1,11 @@
+// Document - is available internally in some way and that internal presentation
+// inside memory is the DOM
+
+// DOM is an interface which is accessible by programming languages like
+// Javascript or Python
+
+// Searching, adding, removing through the DOM tree
+// is called as DOM manipulation
+
+// DOM is an internal model of document where every element of the
+// document is an object and available for programming languages to access
