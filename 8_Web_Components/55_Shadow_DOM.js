519dc671077dd21b07c1455156ec2c0ff87614dc
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-13 14:58:26 +0500

    Problem without shadow DOM

---
+++ 8_Web_Components/55_Shadow_DOM.js
@@ -0,0 +1,3 @@
+// Shadow DOM is a way to provide encapsulation to the web component
+// or in other words - shadow DOM is a way to provide seperate hidden DOM
+// to web components i.e, web component's own DOM
