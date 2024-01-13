dbe10113b121d9f8b1872b2be0af79aede4d4550
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-13 15:11:44 +0500

    Shadow DOM to the rescue

--- 8_Web_Components/55_Shadow_DOM.js
+++ 8_Web_Components/55_Shadow_DOM.js
@@ -1,3 +1,28 @@
 // Shadow DOM is a way to provide encapsulation to the web component
 // or in other words - shadow DOM is a way to provide seperate hidden DOM
 // to web components i.e, web component's own DOM
+
+// Shadow DOM is its own scope for a web component in the DOM
+
+// this.attachShadow({mode: 'open'}); - attaches the component to the DOM
+// When we have to add elements of the web component to the shadow DOM
+// we are going to use the shadowRoot()
+
+class ProductCard extends HTMLElement {
+  constructor() {
+    super();
+    const h1 = document.createElement('h1');
+    h1.innerHTML = `
+        <style>
+            h1{
+                color : red;
+            }
+        </style>
+        Product Component
+        `;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(h1);
+  }
+}
+
+window.customElements.define('product-card', ProductCard);
