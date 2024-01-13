3742fdea4bd828c78f9e4fbad529aebc48661915
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-13 14:35:19 +0500

    Web components introduction

---
+++ 8_Web_Components/53_Introduction.js
@@ -0,0 +1,7 @@
+// In modern web development, there is a very commonly used term called
+// component based architecture
+// Component based architecture is "the" approach most of the
+// modern frameworks also implement
+// The functionalities of a web page are divided into smaller segments
+// and that is what a component based architecture is
+// To implement this architecture we should create "web components"
