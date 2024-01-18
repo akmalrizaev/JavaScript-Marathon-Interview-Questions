5b8390b1344f22d7ba42d02c5d837a20b25b04a1
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-01-18 16:51:33 +0500

    Debouncing Implementation

---
+++ 11_Debounce_Throttle/78_Debouncing_Implementation.js
@@ -0,0 +1,50 @@
+{
+  /* <div onmousemove="showCounts()">
+  <h2 id="nCnt">Normal Count: 0</h2>
+  <h2 id="tCnt">Throttle Count: 0</h2>
+  <h2 id="dCnt">Debounce Count: 0</h2>
+</div>; */
+}
+{
+  /* <script>
+  let nCnt=0; 
+  let tCnt=0; 
+  let dCnt=0; 
+  let nh2 = document.getElementById("nCnt");
+  let th2 = document.getElementById("tCnt"); 
+  let dh2 = document.getElementById("dCnt"); 
+  let fireThrottle = true;
+  let dTimer;
+
+  function showCounts() {
+	normalCounts();
+	throttleCount();
+  }
+
+  function normalCounts() {
+	nCnt++;
+	nh2.innerHTML = "Normal Count:" + nCnt;
+  }
+
+  function throttleCount() {
+	if(fireThrotle==true) {
+
+		tCnt++;
+		th2.innerHTML = "Throttle Count" + tCnt;
+		firThrottle = false;
+		setTimeout(function() {
+             fireThrottle=true;
+		}, 500)
+	}
+  }
+
+  function debounceCount() {
+	window.clearTimeout(dTimer);
+	  dTimer = setTimeout(function() {
+		dCnt++;
+		dh2.innerHTML = "Debounce Count" + dCnt;
+
+	},500);
+  }
+</script>; */
+}
